dab61bbde7a31a7dd462326c237aa21fe1455dff
sri ram <sriramcali@gmail.com>
2020-06-27 14:28:49 -0700

    commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,7 +30,8 @@ const Layout = ({ children }) => {
         style={{
           margin: `0 auto`,
           maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
         }}
       >
         <main>{children}</main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,66 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useEffect, useRef, useState } from "react"
+import TwilioVideo from "twilio-video"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import StartForm from "../components/start-form"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <p>
+const Video = ({ token }) => {
+  const localVidRef = useRef()
+  const remoteVidRef = useRef()
+
+  useEffect(() => {
+    TwilioVideo.connect(token, { video: true, audio: true, name: "test" }).then(
+      room => {
+        // Attach the local video
+        TwilioVideo.createLocalVideoTrack().then(track => {
+          localVidRef.current.appendChild(track.attach())
+        })
+
+        const addParticipant = participant => {
+          console.log("new participant!")
+          console.log(participant)
+          participant.tracks.forEach(publication => {
+            if (publication.isSubscribed) {
+              const track = publication.track
+
+              remoteVidRef.current.appendChild(track.attach())
+              console.log("attached to remote video")
+            }
+          })
+
+          participant.on("trackSubscribed", track => {
+            console.log("track subscribed")
+            remoteVidRef.current.appendChild(track.attach())
+          })
+        }
+
+        room.participants.forEach(addParticipant)
+        room.on("participantConnected", addParticipant)
+      }
+    )
+  }, [token])
+
+  return (
+    <div>
+      <div ref={localVidRef} />
+      <div ref={remoteVidRef} />
+    </div>
+  )
+}
+
+const IndexPage = () => {
+  const [token, setToken] = useState(false)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {!token ? <StartForm storeToken={setToken} /> : <Video token={token} />}
+      <p>
         TODO: 1. Show local video 2. Connect to a room 3. Show participants’
         video (remote) 4. Handle events
       </p>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default IndexPage
